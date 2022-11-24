dbc53a16ef3221877f66f73e4d91e80fa2620d1d
rajibdas-it <rajibrad@gmail.com>
2022-11-24 17:34:21 +0600

    user store api created

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-
 const app = express();
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(express.json());
 app.use(cors());
@@ -11,6 +11,29 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Bikers Ocean Server is running.");
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fe8xrlp.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const usersCollection = client.db("bikersOcean").collection("users");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`Bikers Ocean server is running on port ${port}`);
